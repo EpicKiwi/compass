54f3762bbee099eac9d553360aa5e5a6b76a4099
EpicKiwi <me@epickiwi.fr>
2019-11-26 08:58:30 +0100

    Ajout de l'etat et tentative de Bluetooth

--- app/src/main/java/com/sevencrayons/compass/CompassActivity.java
+++ app/src/main/java/com/sevencrayons/compass/CompassActivity.java
@@ -1,13 +1,23 @@
 package com.sevencrayons.compass;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothServerSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.UUID;
 
 
 public class CompassActivity extends AppCompatActivity {
@@ -18,19 +28,59 @@ public class CompassActivity extends AppCompatActivity {
     private ImageView arrowView;
     private TextView sotwLabel;  // SOTW is for "side of the world"
 
+    private TextView stateText;
+
     private float currentAzimuth;
     private SOTWFormatter sotwFormatter;
+    private BluetoothServerSocket btServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getSupportActionBar().setElevation(0);
+        }
+
         sotwFormatter = new SOTWFormatter(this);
 
         arrowView = findViewById(R.id.main_image_hands);
         sotwLabel = findViewById(R.id.sotw_label);
+        stateText = findViewById(R.id.state_text);
         setupCompass();
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        configureBt();
+
+    }
+
+    protected void configureBt(){
+
+        BluetoothAdapter bluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+        if (bluetoothAdapter == null) {
+            stateText.setText("Pas de Bluetooth");
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            stateText.setText("Bluetooth désactivé");
+        }
+
+        BluetoothManager btManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        try {
+            this.btServer = btManager.getAdapter().listenUsingRfcommWithServiceRecord("RootMapCompass", UUID.fromString("d6bf9198-61ba-477d-99cf-46ff36115011"));
+
+            stateText.setText("En attente de connexion Bluetooth");
+            this.btServer.accept();
+
+        } catch (IOException e) {
+            stateText.setText("Érreur Bluetooth");
+            Log.e("bluetooth",e.getMessage());
+            return;
+        }
+
+        stateText.setText("Mesure en cours");
+
     }
 
     @Override
@@ -50,6 +100,7 @@ public class CompassActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         compass.start();
+        configureBt();
     }
 
     @Override
@@ -89,6 +140,9 @@ public class CompassActivity extends AppCompatActivity {
         return new Compass.CompassListener() {
             @Override
             public void onNewAzimuth(final float azimuth) {
+
+
+
                 // UI updates only in UI thread
                 // https://stackoverflow.com/q/11140285/444966
                 runOnUiThread(new Runnable() {
